afae6b57e733e443d3288c86bc8750df69f7bf13
Ben Schwabe <bspymaster@gmail.com>
2015-01-06 19:54:22 -0600

    documented grid and dice
    Grid.py
    +added documentation to all methods in the grid class
    
    Dice.py
    +added documentation to all methods in the dice class

--- src/grid/Grid.py
+++ src/grid/Grid.py
@@ -1,6 +1,11 @@
 #grid.py
 
 class Grid:
+    """
+    Creates a new instance of a Grid object for putting objects on
+    @param integer the number of rows of the grid
+    @param integer the number of columns of the grid
+    """
     def __init__(self,width,height):
         self.width = width
         self.height = height
@@ -8,40 +13,79 @@ class Grid:
         #populate grid will null objects
         self.grid = [[None for i in range(0,width)] for j in range(0,height)]
     
-    #replaces an object in the grid with None, returns the object removed
+    """
+    replaces an object in the grid with None, returns the object removed
+    @param integer the row number of the object to remove
+    @param integer the column number of the object to remove
+    @return object the object removed
+    """
     def removeObject(self,x,y):
         return self.setLocation(x,y,None)
     
-    #moves the object to end location, deleting current object there and replacing the start location with None, returns the overwritten object
+    """
+    moves the object to end location, deleting current object there and replacing the start location with None
+    @param integer the row of the object being moved
+    @param integer the column of the object being moved
+    @param integer the row to move the object to
+    @param integer the column to move the object to
+    @return object the overwritten object
+    """
     def moveObject(self,startX,startY,endX,endY):
         removedObject = self.setLocation(endX,endY,self.getLocation(startX,startY))
         self.removeObject(startX,startY)
         return removedObject
     
-    #sets the location to the obj and returns what was currently there
+    """
+    sets the location to the obj
+    @param integer the row to set
+    @param integer the column to set
+    @param object the object to set at the desired row and column
+    @return object the object that was there before the location was set
+    """
     def setLocation(self,x,y,obj):
         removedObject = self.getLocation(x,y)
         self.getGrid()[x][y] = obj
         return removedObject
     
-    #switches the objects in the two locations
+    """
+    switches the objects in the two locations
+    @param integer the row of the first location to swap
+    @param integer the column of the first location to swap
+    @param integer the row of the second location to swap
+    @param integer the column of the second location to swap
+    @return boolean always returns true
+    """
     def swapLocation(self,X1,Y1,X2,Y2):
         tempObject = self.moveObject(X1,Y1,X2,Y2)
         self.setLocation(X1,Y1,tempObject)
         return True
     
-    #returns with of grid
+    """
+    gets the width of the grid
+    @return integer the number of rows in the grid
+    """
     def getWidth(self):
         return self.width
     
-    #returns height of grid
+    """
+    gets the height of the grid
+    @return integer the number of columns in the grid
+    """
     def getHeight(self):
         return self.height
     
-    #returns grid
+    """
+    gets the grid object
+    @return Grid the grid object
+    """
     def getGrid(self):
         return self.grid
     
-    #gets object located at target location
+    """
+    gets the object located at the target location
+    @param integer the row of the desired location
+    @param integer the column of the desired location
+    @return object the object located at the desired row and column
+    """
     def getLocation(self,x,y):
         return self.getGrid()[x][y]
--- src/randomNums/Dice.py
+++ src/randomNums/Dice.py
@@ -4,14 +4,21 @@ from random import *
 class Dice:
     dice = {"d2": (1,2), "d6": (1,6), "d10": (1,10), "d12": (1,12), "d20": (1,20), "d100": (1,100)}
     
-    #rolls one die of the selected type
-    #RETURN int result of roll
+    """
+    rolls one die of the selected type
+    @param string the selected die (must be one of the following: "d2", "d6", "d10", "d12", "d20", "d100")
+    @return integer result of roll
+    """
     @classmethod
     def basicDieRoll(self,dieType):
         return randint(self.dice[dieType][0],self.dice[dieType][1])
     
-    #rolls varius types of dice, and adds on modifiers at the end
-    #RETURN tuple (total of all rolls and modifiers,list of all numbers rolled,list of all modifiers)
+    """
+    rolls varius types of dice, and adds on modifiers at the end
+    @param list a list of strings of the selected die (must be one of the following: "d2", "d6", "d10", "d12", "d20", "d100")
+    @param list a list of integers that will modify a die roll
+    @return tuple an integer total of all rolls and modifiers, followed by list of integers of all the numbers rolled, followed by a list of integers of all the modifiers
+    """
     @classmethod
     def complexDieRoll(self,dieTypes,modifiers):
         total = 0
